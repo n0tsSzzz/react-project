e89b34612834a14ddfa42f2f79f510c3ea283d2c
Marko <90ts@mail.ru>
2024-12-02 10:31:44 +0300

    feat: add auth checks

--- src/pages/service-detail-page/ServiceDetailPage.jsx
+++ src/pages/service-detail-page/ServiceDetailPage.jsx
@@ -4,33 +4,40 @@ import MyCard from '../../components/card/Card';
 
 
 const ServiceDetail = () => {
-    const { id } = useParams();
-    const [service, setService] = useState(null);
-    useEffect(() => {
-      fetch(`https://673423afa042ab85d1190055.mockapi.io/api/v1/services/${id}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          setService(data);
-        })
-        .catch((error) => {
-          console.error('Error fetching the service:', error);
-        });
-    }, [id]);
-    return (
-      <>
-        {service ? (
-          <MyCard 
-            imgURI={service.image} 
-            name={service.name} 
-            desc={service.description} 
-            id={service.id} 
-          />
-        ) : (
-          <div>Услуга не найдена.</div>
-        )}
-      </>
-    );
-  };
-  export default ServiceDetail;
+  const { id } = useParams();
+  const [service, setService] = useState(null);
+
+  const currentUserID = localStorage.getItem('id')
+
+  useEffect(() => {
+    fetch(`https://673423afa042ab85d1190055.mockapi.io/api/v1/services/${id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setService(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching the service:', error);
+      });
+  }, [id]);
+
+  return (
+    <>
+      {!currentUserID ? (
+        <div style={{textAlign: "center"}}>Вы должны войти в аккаунт.</div>
+      ) : service ? (
+        <MyCard 
+          imgURI={service.image} 
+          name={service.name} 
+          desc={service.description} 
+          id={service.id} 
+        />
+      ) : (
+        <div>Услуга не найдена.</div>
+      )}
+    </>
+  );
+};
+
+export default ServiceDetail;
