a7b1940f6168826a4f57048ddc6f4f1bcc9cb20a
Marko <90ts@mail.ru>
2024-12-02 09:49:25 +0300

    feat: added authentication

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,21 +1,93 @@
 import React from "react";
 import './LoginPage.css'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import {Text} from "@consta/uikit/Text"
+
+const AUTH_TOKEN_KEY_NAME = 'lk-sirius-token';
+const REFRESH_TOKEN_KEY_NAME = 'lk-sirius-token';
+
+export type Token = string;
+
+export const getToken = () => {
+    const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
+    return token ?? '';
+};
+
+export const saveTokens = (accessToken: Token, refreshToken: Token) => {
+    localStorage.setItem(AUTH_TOKEN_KEY_NAME, accessToken);
+    localStorage.setItem(REFRESH_TOKEN_KEY_NAME, refreshToken);
+};
+
+
+export const saveProfileData = (userData) => {
+    localStorage.setItem('id', userData.id);
+}
+
+export const dropToken = () => {
+    localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+};
 
 const LoginPage = () => {
     const [formData, setFormData] = React.useState({
         username: '',
         password: ''
-    });
-    
-    
+    });    
+    const [invalidInput, setInvalidInput] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    const navigate = useNavigate();
+
     function updateFormData (e) {
         const {name, value} = e.target;
         setFormData({...formData, [name]: value})
     }
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        console.log(formData.username)
+        
+        try {
+            if (!formData.username || !formData.password) {
+                setInvalidInput(true)
+                return
+            } else {
+                setInvalidInput(false)
+            }
+            
+            const resp = await fetch('https://dummyjson.com/auth/login', {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ username: formData.username, password: formData.password, expiresInMins: 100 })
+            })
+
+            if (!resp.ok) {
+                console.log(resp.status);
+                console.log(formData.username, formData.password);
+                throw new Error('Ошибка аутентификации');
+            }
+
+            const loginData = await resp.json()
+            let [accessToken, refreshToken] = [loginData['accessToken'], loginData['refreshToken']]
+
+            saveTokens(accessToken, refreshToken)
+            
+            const getMeResp = await fetch('https://dummyjson.com/auth/me', {
+                method: "GET",
+                headers: {'Authorization': `Bearer ${accessToken}`},
+            })
+
+            const userData = await getMeResp.json()
+            
+            console.log(userData)
+
+            saveProfileData(userData)
+            navigate(`/user/${userData.id}`);
+            window.location.reload();
+        } catch (error) {
+            setError(error.message);
+        }
     }
 
     return (
@@ -25,7 +97,7 @@ const LoginPage = () => {
                         <form className="login" onSubmit={handleSubmit}>
                             <div className="login__field">
                                 <i className="login__icon fas fa-user"></i>
-                                <input type="text" name="username" className="login__input" placeholder="Юзернейм / Почта" onChange={updateFormData}/>
+                                <input type="text" name="username" className="login__input" placeholder="Логин" onChange={updateFormData}/>
                             </div>
                             <div className="login__field">
                                 <i className="login__icon fas fa-lock"></i>
@@ -35,14 +107,17 @@ const LoginPage = () => {
                                 <span className="button__text">Войти</span>
                                 <i className="button__icon fas fa-chevron-right"></i>
                             </button>
+                            { invalidInput ? 
+                                <Text view="alert">Заполните поля</Text>
+                            : null}
+                            {error && <Text align="left" view="alert">{error}</Text>}
                         </form>
-
                     </div>
                     <div className="screen__background">
-                        <span className="screen__background__shape screen__background__shape4"></span>
-                        <span className="screen__background__shape screen__background__shape3"></span>
-                        <span className="screen__background__shape screen__background__shape2"></span>
-                        <span className="screen__background__shape screen__background__shape1"></span>
+                            <span className="screen__background__shape screen__background__shape4"></span>
+                            <span className="screen__background__shape screen__background__shape3"></span>
+                            <span className="screen__background__shape screen__background__shape2"></span>
+                            <span className="screen__background__shape screen__background__shape1"></span>
                     </div>
                 </div>
             </div>
